3cb426d5837f781706be847135fd2cc52ecead4f
Watanabe Hiroki <hiroki@WatanabenoMacBook-puro.local>
2019-10-24 01:49:11 +0900

    バリデーションの設定

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -6,14 +6,21 @@ class BoardsController < ApplicationController
   end
 
   def new
-    @board = Board.new
+    @board = Board.new(flash[:board])
   end
 
   def create
-    board = Board.create(board_params)
+  board = Board.new(board_params)
+  if board.save
     flash[:notice] = "「#{board.title}」の掲示板を作成しました"
     redirect_to board
+  else
+    redirect_to new_board_path, flash: {
+      board: board,
+      error_messages: board.errors.full_messages
+    }
   end
+ end
 
   def show
   end
