9d33c591ab500b714ab641d7d670b3f599e8561d
Watanabe Hiroki <hiroki@WatanabenoMacBook-puro.local>
2019-10-24 01:41:40 +0900

    ページネーションの修正

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,6 @@ module DevWebapp
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.time_zone = 'Tokyo'
-    config.i18n.default_locale = ja
+    config.i18n.default_locale = :ja
   end
 end
