e8876e8cbb5c09515a1851cf6387a04ac1e618a4
Watanabe Hiroki <hiroki@WatanabenoMacBook-puro.local>
2019-10-24 01:43:12 +0900

    フラッシュの適用

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,20 +1,21 @@
 class BoardsController < ApplicationController
-before_action :set_target_board, only: %i[show edit update destroy]
+  before_action :set_target_board, only: %i[show edit update destroy]
 
   def index
     @boards = Board.page(params[:page])
   end
 
   def new
-    @board = Board.new 
+    @board = Board.new
   end
 
   def create
     board = Board.create(board_params)
+    flash[:notice] = "「#{board.title}」の掲示板を作成しました"
     redirect_to board
   end
 
-  def show 
+  def show
   end
 
   def edit
@@ -27,16 +28,15 @@ before_action :set_target_board, only: %i[show edit update destroy]
 
   def destroy
     @board.delete
-    redirect_to boards_path
-   
+    redirect_to boards_path, flash: { notice: "「#{@board.title}」の掲示板が削除されました" }
   end
 
   private
-  
+
   def board_params
     params.require(:board).permit(:name, :title, :body)
   end
-  
+
   def set_target_board
     @board = Board.find(params[:id])
   end
