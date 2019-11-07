7cb8e20c82163150461420c5bf35d9d41d54280d
Watanabe Hiroki <hiroki@WatanabenoMacBook-puro.local>
2019-11-08 01:55:11 +0900

    create comment controller

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,7 @@
 class Board < ApplicationRecord
+  has_many :comments
+
+
   validates :name, presence: true, length: { maximum: 10 }
   validates :title, presence: true, length: { maximum: 30 }
   validates :body, presence: true, length: { maximum: 1000 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_22_094207) do
+ActiveRecord::Schema.define(version: 2019_11_07_164452) do
 
   create_table "boards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -20,10 +20,20 @@ ActiveRecord::Schema.define(version: 2019_10_22_094207) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "board_id"
+    t.string "name", null: false
+    t.text "comment", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["board_id"], name: "index_comments_on_board_id"
+  end
+
   create_table "todos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "boards"
 end
