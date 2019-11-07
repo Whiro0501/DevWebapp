f5b92920d27ea21876c62c4d5e59bc923f0d4581
Watanabe Hiroki <hiroki@WatanabenoMacBook-puro.local>
2019-11-08 02:12:31 +0900

    edit comment controller

--- Gemfile
+++ Gemfile
@@ -49,6 +49,7 @@ gem 'rails-i18n'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'rails-flog', require: 'flog'
 end
 
 group :development do
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,22 @@
 class CommentsController < ApplicationController
   def create
+    comment = Comment.new(comment_params)
+    if comment.save
+      flash[:notice] = 'コメントを投稿しました'
+      redirect_to comment.board
+    else
+      flash[:comment] = comment
+      flash[:error_messages] = comment.errors.full_messages
+      redirect_back fallback_location: comment.board
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:board_id, :name, :comment)
   end
 end
