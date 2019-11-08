02045aa806fba65ade3534c1eb107e7ac6a7bc60
Watanabe Hiroki <hiroki@WatanabenoMacBook-puro.local>
2019-11-08 16:37:55 +0900

    add header menu

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,6 +2,7 @@ class BoardsController < ApplicationController
   before_action :set_target_board, only: %i[show edit update destroy]
 
   def index
+    @boards = params[:tag_id].present? ? Tag.find(params[:tag_id]).boards : Board.all
     @boards = Board.page(params[:page])
   end
 
