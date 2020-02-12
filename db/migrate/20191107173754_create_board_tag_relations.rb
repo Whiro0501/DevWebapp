ca4bb5c567b3ff6ffa5a0ce092efe7a137dd7dd5
Whiro0501 <36604680+Whiro0501@users.noreply.github.com>
2020-02-12 23:46:10 +0900

    Update 20191107173754_create_board_tag_relations.rb

--- db/migrate/20191107173754_create_board_tag_relations.rb
+++ db/migrate/20191107173754_create_board_tag_relations.rb
@@ -3,7 +3,6 @@ class CreateBoardTagRelations < ActiveRecord::Migration[5.2]
     create_table :board_tag_relations do |t|
       t.references :board, foreign_key: true
       t.references :tag, foreign_key: true
-
       t.timestamps
     end
   end
